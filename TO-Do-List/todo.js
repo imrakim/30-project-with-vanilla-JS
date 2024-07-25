91a913c989eea5f481d8935f841040956e621a0b
rakim <rakimali770@gmail.com>
2024-07-25 23:17:34 +0600

    adding js

--- TO-Do-List/todo.js
+++ TO-Do-List/todo.js
@@ -1,1 +1,13 @@
-let 
+const inputBox = document.getElementById("input");
+const listItem = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value===''){
+        alert("!You Must Write Something")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listItem.appendChild(li);
+    }
+}
