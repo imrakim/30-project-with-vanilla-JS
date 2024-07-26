d688d18464a89f48a45ef82fa68293ee94a409da
rakim <rakimali770@gmail.com>
2024-07-27 01:56:03 +0600

    fix javascript problem

--- Quiz-app/quiz.js
+++ Quiz-app/quiz.js
@@ -1,38 +1,132 @@
+// const questions = [
+//   {
+//     Questions: "Which is the largest Animal In the world",
+//     answer: [
+//       { text: "shark", correcct: false },
+//       { text: "Blue While", correcct: true },
+//       { text: "Elephant", correcct: false },
+//       { text: "Gerraf", correcct: false },
+//     ],
+//   },
+//   {
+//     Questions: "Who is The Founder of Javascript",
+//     answer: [
+//       { text: "Brendan Eich", correcct: true },
+//       { text: "Mark Zuckerberg", correcct: false },
+//       { text: "Elon Musk", correcct: false },
+//       { text: "Bill Gates", correcct: false },
+//     ],
+//   },
+//   {
+//     Questions: "When Fouwhen microsoft founded",
+//     answer: [
+//       { text: "April 4, 1976", correcct: false },
+//       { text: "April 14, 1977", correcct: false },
+//       { text: "April 4, 1975", correcct: true },
+//       { text: "April 24, 1965", correcct: false },
+//     ],
+//   },
+//   {
+//     Questions: "who is the founder of facebook",
+//     answer: [
+//       { text: "Brendan Eich", correcct: false },
+//       { text: " Mark Zuckerberg", correcct: true },
+//       { text: "Elon Musk", correcct: false },
+//       { text: "Bill Gates", correcct: false },
+//     ],
+//   },
+// ];
+
+// const questionsElement = document.getElementById("questions");
+// const answerButton = document.getElementById("answer-button");
+// const nextButton = document.getElementById("next-btn");
+
+// let currentQuestionsIndex = 0;
+// let score = 0;
+
+// function startQuiz() {
+//   currentQuestionsIndex = 0;
+//   score = 0;
+//   nextButton.innerHTML = "NEXT";
+//   showQuestions();
+// }
+
+// function showQuestions() {
+//   resetState();
+//   let currentQuestions = questions[currentQuestionsIndex];
+//   let questionNO = currentQuestionsIndex + 1;
+//   questionsElement.innerHTML = questionNO + "." + currentQuestions.Questions;
+
+//   currentQuestions.answer.forEach((answer) => {
+//     const button = document.createElement("button");
+//     button.innerHTML = answer.text;
+//     button.classList.add("btn");
+//     answerButton.appendChild(button);
+//     if(button.correcct){
+//       button.dataset.correcct=answer.correcct;
+//     }
+//     button.addEventListener("click", selectAnswer);
+//   });
+// };
+
+// function resetState(){
+//   nextButton.style.display="none";
+//   while(answerButton.firstChild){
+//     answerButton.removeChild(answerButton.firstChild);
+//   }
+// };
+
+// function selectAnswer(e){
+//   const selectedbtn = e.target;
+//   const isCorrect = selectedbtn.dataset.correcct==="true";
+//   if(isCorrect){
+//     selectedbtn.classList.add("correcct")
+//   }else{
+//     selectedbtn.classList.add("incorrect")
+//   }
+
+// }
+// startQuiz();
+
+
+
+// chatgpt version
+
 const questions = [
   {
-    Questions: "Which is the largest Animal In the world",
-    answer: [
-      { text: "shark", correcct: false },
-      { text: "Blue While", correcct: true },
-      { text: "Elephant", correcct: false },
-      { text: "Gerraf", correcct: false },
+    question: "Which is the largest Animal In the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
     ],
   },
   {
-    Questions: "Who is The Founder of Javascript",
-    answer: [
-      { text: "Brendan Eich", correcct: true },
-      { text: "Mark Zuckerberg", correcct: false },
-      { text: "Elon Musk", correcct: false },
-      { text: "Bill Gates", correcct: false },
+    question: "Who is the Founder of JavaScript?",
+    answers: [
+      { text: "Brendan Eich", correct: true },
+      { text: "Mark Zuckerberg", correct: false },
+      { text: "Elon Musk", correct: false },
+      { text: "Bill Gates", correct: false },
     ],
   },
   {
-    Questions: "When Fouwhen microsoft founded",
-    answer: [
-      { text: "April 4, 1976", correcct: false },
-      { text: "April 14, 1977", correcct: false },
-      { text: "April 4, 1975", correcct: true },
-      { text: "April 24, 1965", correcct: false },
+    question: "When was Microsoft founded?",
+    answers: [
+      { text: "April 4, 1976", correct: false },
+      { text: "April 14, 1977", correct: false },
+      { text: "April 4, 1975", correct: true },
+      { text: "April 24, 1965", correct: false },
     ],
   },
   {
-    Questions: "who is the founder of facebook",
-    answer: [
-      { text: "Brendan Eich", correcct: false },
-      { text: " Mark Zuckerberg", correcct: true },
-      { text: "Elon Musk", correcct: false },
-      { text: "Bill Gates", correcct: false },
+    question: "Who is the founder of Facebook?",
+    answers: [
+      { text: "Brendan Eich", correct: false },
+      { text: "Mark Zuckerberg", correct: true },
+      { text: "Elon Musk", correct: false },
+      { text: "Bill Gates", correct: false },
     ],
   },
 ];
@@ -53,37 +147,39 @@ function startQuiz() {
 
 function showQuestions() {
   resetState();
-  let currentQuestions = questions[currentQuestionsIndex];
+  let currentQuestion = questions[currentQuestionsIndex];
   let questionNO = currentQuestionsIndex + 1;
-  questionsElement.innerHTML = questionNO + "." + currentQuestions.Questions;
+  questionsElement.innerHTML = questionNO + ". " + currentQuestion.question;
 
-  currentQuestions.answer.forEach((answer) => {
+  currentQuestion.answers.forEach((answer) => {
     const button = document.createElement("button");
     button.innerHTML = answer.text;
     button.classList.add("btn");
     answerButton.appendChild(button);
-    if(button.correcct){
-      button.dataset.correcct=answer.correcct;
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
     }
     button.addEventListener("click", selectAnswer);
   });
-};
+}
 
-function resetState(){
-  nextButton.style.display="none";
-  while(answerButton.firstChild){
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButton.firstChild) {
     answerButton.removeChild(answerButton.firstChild);
   }
-};
-
-function selectAnswer(e){
-  const selectedbtn = e.target;
-  const isCorrect = selectedbtn.dataset.correcct==="true";
-  if(isCorrect){
-    selectedbtn.classList.add("correcct")
-  }else{
-    selectedbtn.classList.add("incorrect")
-  }
-
 }
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const isCorrect = selectedButton.dataset.correct === "true";
+  if (isCorrect) {
+    selectedButton.classList.add("correct");
+  } else {
+    selectedButton.classList.add("incorrect");
+  }
+}
+
 startQuiz();
+
+
