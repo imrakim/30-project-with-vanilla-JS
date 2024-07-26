1028ee48047670a3f568b5396edf3a849891334f
rakim <rakimali770@gmail.com>
2024-07-27 02:05:39 +0600

    finally 1st part complate

--- Quiz-app/quiz.js
+++ Quiz-app/quiz.js
@@ -178,6 +178,13 @@ function selectAnswer(e) {
   } else {
     selectedButton.classList.add("incorrect");
   }
+  Array.from(answerButton.children).forEach(button =>{
+    if(button.dataset.correct==="true"){
+      button.classList.add("correct");
+    };
+    button.disiabled=true;
+  });
+  nextButton.style.display="block"
 }
 
 startQuiz();
